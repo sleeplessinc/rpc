d54a438968166458ebfc0521600f8b0accb74b44
Joe Hitchens <joe@sleepless.com>
2021-04-26 19:01:35 -0700

    -

--- index.js
+++ index.js
@@ -1,159 +1,46 @@
-/*
-Copyright 2013 Sleepless Software Inc. All rights reserved.
+// Copyright 2021 Sleepless Software Inc. All rights reserved.
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to
-deal in the Software without restriction, including without limitation the
-rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-sell copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+// delete require.cache[module.filename]; // uncomment for testing
+const HERE = require("path").dirname( module.filename );
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+require( "sleepless" ).globalize();
+const	connect			= require("connect"),
+		serve_static 	= require( "serve-static" ),
+		L = log5.mkLog( "\tmicro_server: " )(5);
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-IN THE SOFTWARE. 
+const app = connect();
 
-*/
+app.use( require( "body-parser" ).json() );
+app.use( require( "cookie-parser" )() );
+app.use( require( "compression" )() );
+//app.use( require( "cors" )() );	// enable to handle requests from other domains
+app.use( mw_fin_json );		// adds okay, fail to res (defined in sleepless module)
+app.use( ( req, res, next ) => {
+	req.query = require('querystring').parse( req._parsedUrl.query );
+	next();
+});
 
-var util = require("util")
-var url = require("url")
-var http = require("http")
-var log = require("log5").mkLog("rpc:")
-
-exports.log = log
-
-var j2o = function(j) { try { return JSON.parse(j) } catch(e) { return null } }
-var o2j = function(o) { return JSON.stringify(o) }
-
-var nop = function(){}
-
-
-function messageEnd(tx, msgOut) {
-
-	var json = o2j(msgOut)
-
-	log(3, "<<< "+json)
-
-	tx.res.writeHead(200, {
-		"Cache-Control": "no-cache",
-		"Content-Type": "text/plain",
-		"Content-Length": "" + json.length,
-	})
-
-	tx.res.end(json)
-
-}
-
-
-function messageStart(tx, json) {
-
-	var msg = j2o(json)
-
-	log(3, ">>> "+json)
-
-	tx.json = json
-	tx.msg = msg
-
-	if(!msg)
-		return fail(tx, "no message")
-
-	if(typeof msg !== "object")
-		return fail(tx, "bad message")
-
-	var args = msg.args
-	if(!(args instanceof Array))
-		return fail(tx, "message not an array")
-
-	if(args.length < 1)
-		return fail(tx, "array empty")
-
-	var funcName = args[0].trim()
-	if(!funcName)
-		return fail(tx, "no function")
-
-	var cbs = tx.cbs
-	var func = cbs[funcName]
-	if(!func)
-		return fail(tx, "function not found: "+funcName)
-
-	args.splice(0, 1, function(msgOut){messageEnd(tx, msgOut)})
-	func.apply(tx, args)
-
-}
-
-
-function fail(tx, why) {
-
-	why = why || "mystery"
-	log(3, "FAIL: "+why)
-	var msg = {error:why}
-	messageEnd(tx, msg)
-
-}
-
-
-function messageInit(tx) {
-
-	var m = tx.req.method
-	var json = tx.query.j
-
-	if(!json) {
-		tx.res.writeHead(404, {
-			"Cache-Control": "no-cache",
-			"Content-Type": "text/plain"
-		})
-		tx.res.end(tx.path + ": NOT FOUND")
-		log(3, "NOT FOUND: " + tx.path)
-		return
-	}
-
-	if(m == "GET") {
-		messageStart(tx, json)
-		return
-	}
-
-	if(m == "POST") {
-		tx.req.pause()		// necessary?
-		messageStart(tx, json)
-		return
-	}
-
-	fail(tx, "bad method "+m)
-
-}
-
-
-// Every request starts here
-function accept(req, res, cbs) {
-
-	if(req.method == "OPTIONS") {
-		res.writeHead(200, {
-			"Access-Control-Allow-Origin": "*",	// to allow cross-domain script execution 
-			"Access-Control-Max-Age": "0",
-		})
-		res.end()
+// API path handler
+app.use( ( req, res, next ) => {
+	const { method, url, query, body } = req;
+	L.V( method + " " + url );
+	if( /GET|POST/.test( method ) && url.startsWith( "/api/" ) ) {
+		try {
+			const path = HERE + "/api";
+			const mod = require( path );
+			const input = ( method == "GET" ) ? query : body;
+			mod( input, res.okay, res.fail );	
+		} catch( e ) {
+			L.E( e.stack );
+			next();
+		}
 		return;
 	}
-	
-	var u = url.parse(req.url, true)
-	var path = u.pathname
-	var tx = {cbs:(cbs || nop), req:req, res:res, path:path, query:u.query, u:u}
-	messageInit(tx)
+	next();
+});
 
-}
+// Serve static files
+app.use( serve_static( HERE + "/static" ) );
 
-
-exports.createServer = function(cbs) {
-
-	return http.createServer(function(req, res) {
-		accept(req, res, cbs)
-	}) 
-
-}
+module.exports = app;
 
