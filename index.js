958b27d0afcb300f254e987569682fe863c29152
Joe Hitchens <joe@sleepless.com>
2022-07-13 16:33:08 -0700

    -

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const HERE = require("path").dirname( module.filename );
 
-require( "sleepless" );
+//require( "sleepless" );
 
 module.exports = function( url_path, api_path, opts = {} ) {
 
@@ -17,7 +17,7 @@ module.exports = function( url_path, api_path, opts = {} ) {
 	if( opts.cors ) {
 		app.use( require( "cors" )() );	// enable to handle requests from other domains
 	}
-	app.use( mw_fin_json );		// adds okay, fail to res (defined in sleepless module)
+	//app.use( mw_fin_json );		// adds okay, fail to res (defined in sleepless module)
 	app.use( ( req, res, next ) => {
 		req.query = require('querystring').parse( req._parsedUrl.query );
 		next();
@@ -31,7 +31,17 @@ module.exports = function( url_path, api_path, opts = {} ) {
 			const path = api_path;
 			const mod = require( path );
 			const input = ( method == "GET" ) ? query : body;
-			mod( input, res.okay, res.fail, req, res );	
+
+			const done = ( error, data ) => {
+				let json = JSON.stringify( { error, data } );
+				res.writeHead( 200, { "Content-Type": "application/json", });
+				res.write( json );
+				res.end();
+			};
+			const fail = ( error, body ) => { done( error, body ); };
+			const okay = ( data ) => { done( null, data ); };
+
+			mod( input, okay, fail, req, res );	
 		} else {
 			// Pass this request on to the next handler
 			next();
